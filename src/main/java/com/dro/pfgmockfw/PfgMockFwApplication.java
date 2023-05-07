1d10a8f1efc7b65397c0896359c66792d853059d
davidrodriguez1317 <davidrodriguez1317@gmail.com>
2023-05-07 07:37:26 +0200

    first commit

---
+++ src/main/java/com/dro/pfgmockfw/PfgMockFwApplication.java
@@ -0,0 +1,13 @@
+package com.dro.pfgmockfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfgMockFwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfgMockFwApplication.class, args);
+    }
+
+}
